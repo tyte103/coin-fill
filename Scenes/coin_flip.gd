3e09b1de07085b84728a21e90b4cfe53fd4ded9e
tyte103 <143618588+tyte103@users.noreply.github.com>
2025-09-27 22:48:03 +0800

    coin flip可依照輸入內容正確展示正反面
    鏡頭和光線角度要改

--- Scenes/coin_flip.gd
+++ Scenes/coin_flip.gd
@@ -1,11 +1,11 @@
 extends Node3D
 
-@export var flip_force_y = 10.0 #向上力度
-@export var flip_torque_x = 150.0 #力矩力度
-@export var spin_time = 3.0 #旋轉時間
+@export var flip_force_y = 8.0 #向上力度
+@export var flip_torque_x = 130.0 #力矩力度
+@export var min_spin_time = 2.0 #旋轉時間
 
 var coin: RigidBody3D
-var is_frontResult = false 
+var is_frontResult = false
 
 #測試開始
 func _ready():
@@ -20,7 +20,7 @@ func toss_coin():
 	
 	#重置硬幣位置
 	coin.global_position = Vector3(0, 1.0, 0)
-	is_frontResult = false #結果匯入
+	is_frontResult = true #結果匯入
 	
 	#向上及旋轉
 	var toss_impulse = Vector3(0, flip_force_y, 0)
@@ -29,9 +29,10 @@ func toss_coin():
 	coin.apply_impulse(toss_impulse)
 	coin.apply_torque(toss_torque)
 	
+	await get_tree().create_timer(min_spin_time).timeout
 	#結果判定
-	await get_tree().create_timer(spin_time).timeout
-	finalize_result()
+	if coin.global_position.y <= 1.5:
+		finalize_result()
 	
 #顯示結果
 func finalize_result():
@@ -39,11 +40,8 @@ func finalize_result():
 	coin.angular_velocity = Vector3.ZERO
 	
 	var final_rotation = Vector3.ZERO
-	
 	if is_frontResult:
 		final_rotation = Vector3(deg_to_rad(0), 0, 0)
-	
 	else:
 		final_rotation = Vector3(deg_to_rad(180), 0, 0)
-	
 	$Coin/MeshInstance3D.rotation = final_rotation
